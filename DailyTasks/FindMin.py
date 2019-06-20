567537421da003bf22b722a68fd39878f0ce26bf
bishwobhandari <bhanbish@isu.edu>
2019-06-20 13:02:10 -0400

    Add files via upload

--- DailyTasks/FindMin.py
+++ DailyTasks/FindMin.py
@@ -1,1 +1,14 @@
+data_list = input("Please enter a list of numbers")
+new_input = data_list.split()
+int_data = [int(x) for x in new_input]
+new_list = []
 
+while int_data:
+    minimum = int_data[0]  # arbitrary number in list 
+    for x in int_data: 
+        if x < minimum:
+            minimum = x
+    new_list.append(minimum)
+    int_data.remove(minimum)    
+
+print(new_list[0])
